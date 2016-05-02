8aff45cc784a23746dbc874e089c13a95f821b86
Claudio Alessandro <claudioalessandro.22@gmail.com>
2016-05-01 21:03:03 -0300

    primeiro commit

---
+++ src/main/java/br/com/claudio/drogaria/util/HibernateUtil.java
@@ -0,0 +1,34 @@
+package br.com.claudio.drogaria.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+	   private static SessionFactory fabricaDeSessoes = criarFabricaDeSessoes();
+	   
+	   public static SessionFactory getFabricaDeSessoes() {
+	        return fabricaDeSessoes;
+	    }
+
+	    private static SessionFactory criarFabricaDeSessoes() {
+	        try {
+	            // Create the SessionFactory from hibernate.cfg.xml
+	            Configuration configuracao = new Configuration().configure();
+	            
+	            ServiceRegistry registro = new StandardServiceRegistryBuilder().applySettings(configuracao.getProperties()).build();
+	            
+	            SessionFactory fabrica = configuracao.buildSessionFactory(registro);
+	            
+	            return fabrica;
+	        }
+	        catch (Throwable ex) {
+	            // Make sure you log the exception, as it might be swallowed
+	            System.err.println("A Fábrica de sessão não pôde ser criada." + ex);
+	            throw new ExceptionInInitializerError(ex);
+	        }
+	    }
+
+	    
+}
